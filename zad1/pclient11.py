659651acd78e9caddb8f6faaa0c036ccf046be87
sawertynn <sawertynn@gmail.com>
2024-11-13 14:25:44 +0100

    better messaging

--- zad1/pclient11.py
+++ zad1/pclient11.py
@@ -5,42 +5,59 @@ import argparse
 
 BUFFER = 1024
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--address", dest='host', help="IP address of host, default localhost", default="127.0.0.1")
-    parser.add_argument("-p", "--port", dest='port', type=int, help="port number, default 8000", default=8000)
-    parser.add_argument("sizes", nargs="+", type=int, help="what sizes of datagrams will be transmitted")
-    
-    return parser.parse_args()
+    parser.add_argument(
+        "-a",
+        "--address",
+        dest="host",
+        help="IP address of host, default localhost",
+        default="127.0.0.1",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        dest="port",
+        type=int,
+        help="port number, default 8000",
+        default=8000,
+    )
+    parser.add_argument(
+        "sizes", nargs="+", type=int, help="what sizes of datagrams will be transmitted"
+    )
 
+    return parser.parse_args()
 
 
 def generate_data(size: int) -> bytes:
     if size < 2:
-        raise ValueError('parameter: size >= 2')
+        raise ValueError("parameter: size >= 2")
 
     binary_stream = io.BytesIO()
-    packed = struct.pack('!H', size)
+    packed = struct.pack("!H", size)
 
     binary_stream.write(packed)
     for i in range(size - 2):  # 2 bytes taken by size
         shift = i % 26
-        character = chr(ord('A') + shift)
-        binary_stream.write(character.encode('ascii'))
+        character = chr(ord("A") + shift)
+        binary_stream.write(character.encode("ascii"))
     binary_stream.seek(0)
     return binary_stream.read()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     args = get_args()
-    
+
+    print("Python client for zadanie 1.1")
     print("Will send to ", args.host, ":", args.port)
+
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         for size in args.sizes:
             data = generate_data(size)
-            print(f'{len(data)=}, {data[0:2]=}')
+            print(f"sending dgram of {size=}")
             s.sendto(data, (args.host, args.port))
 
             response, address = s.recvfrom(BUFFER)
-            print(f'{response=}')
+            print(f"{response=}")
--- zad1/pserver11.py
+++ zad1/pserver11.py
@@ -1,57 +1,70 @@
 import socket
-import io
 import struct
 import argparse
 
 BUFFER = 1 << 16
 
+
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--address", dest='host', help="IP address of host, default localhost", default="127.0.0.1")
-    parser.add_argument("-p", "--port", dest='port', type=int, help="port number, default 8000", default=8000)
-    
+    parser.add_argument(
+        "-a",
+        "--address",
+        dest="host",
+        help="IP address of host, default localhost",
+        default="127.0.0.1",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        dest="port",
+        type=int,
+        help="port number, default 8000",
+        default=8000,
+    )
+
     return parser.parse_args()
 
-def generate_data(size: int) -> bytes:
+
+def validate_data(data: bytes) -> bool:
+    size = len(data)
     if size < 2:
-        raise ValueError('parameter: size >= 2')
+        print("ERROR: datagram too small")
+        return False
+    number = struct.unpack("!H", data[0:2])[0]
 
-    binary_stream = io.BytesIO()
-    packed = struct.pack('!H', size)
+    if size != number:
+        print(
+            f"ERROR: mismatch between supposed size ({number}) \
+            and size of received datagram ({size})"
+        )
+        return False
 
-    binary_stream.write(packed)
-    for i in range(size - 2):  # 2 bytes taken by size
-        shift = i % 26
-        character = chr(ord('A') + shift)
-        binary_stream.write(character.encode('ascii'))
-    binary_stream.seek(0)
-    return binary_stream.read()
-
-
-def validate_data(data: bytes):
-    pass
+    print(f"received good datagram of {size=}")
+    return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     args = get_args()
 
+    print("Python server for zadanie 1.1")
     print("Will listen on ", args.host, ":", args.port)
-
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind((args.host, args.port))
-        i=1
+        i = 1
         while True:
             data, address = s.recvfrom(BUFFER)
 
-            number = struct.unpack('!H', data[0:2])
-            print(f'{number=}, {len(data)=} {data[-5:]}')
+            size = len(data)
+            number = struct.unpack("!H", data[0:2])[0]
 
-            response = f'dgram_#{i}'.encode('ascii')
+            check = validate_data(data)
+
+            check_text = "COR" if check else "ERR"
+            response = f"{check_text}_dgram_#{i}".encode("ascii")
 
             s.sendto(response, address)
-            print(f'sending: {response}')
-            i+=1
-
-
+            print(f"sending: {response}")
+            i += 1
